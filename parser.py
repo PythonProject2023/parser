32014a7c40f99e5b4718294f2485effc8979713e
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2023-06-09 12:38:57 +0300

    Доступ к вопросу и теме теперь по стоимости и названию; парсинг пакета стал функцией

--- parser.py
+++ parser.py
@@ -64,32 +64,32 @@ class Question:
 
 
 class Theme:
-    questions = list()
+    questions = dict()
 
     def __init__(self, name):
         self.name = name
 
     def add_question(self, q):
-        self.questions.append(q)
+        self.questions[q.get_price()] = q
 
-    def get_question(self):
-        yield from self.questions
+    def get_question(self, p):
+        return self.questions[p]
 
     def __str__(self):
         return self.name
 
 
 class Round:
-    themes = list()
+    themes = dict()
 
     def __init__(self, name):
         self.name = name
 
     def add_theme(self, t):
-        self.themes.append(t)
+        self.themes[str(t)] = t
 
-    def get_theme(self):
-        yield from self.themes
+    def get_theme(self, nm):
+        return self.themes[nm]
 
     def __str__(self):
         return self.name
@@ -108,73 +108,50 @@ class Package:
     def add_round(self, round):
         self.rounds.append(round)
 
-    def get_round(self):
-        yield from self.rounds
+    def get_round(self, num):
+        return self.rounds[num]
 
     def __str__(self):
-        return f"{author}\n\n" + '\n'.join(str(i) for i in self.rounds)
+        return f"{self.author}\n\n" + '\n'.join(str(i) for i in self.rounds)
 
 
-directory_path = Path.cwd()
-packet_path = sys.argv[1]
+def parse_package(packet_path):
 
-with zipfile.ZipFile(packet_path) as file:
-    file_list = file.namelist()
-    with file.open(file_list[0]) as xml_file:
-        tree = ET.parse(xml_file)
-        root = tree.getroot()
+    with zipfile.ZipFile(packet_path) as file:
+        file_list = file.namelist()
+        with file.open(file_list[0]) as xml_file:
+            tree = ET.parse(xml_file)
+            root = tree.getroot()
 
-# Получаем пространство имен из корневого элемента
-namespace = {'ns': root.tag.split('}')[0].strip('{')}
+    # Получаем пространство имен из корневого элемента
+    namespace = {'ns': root.tag.split('}')[0].strip('{')}
 
-p = Package()
+    p = Package()
 
-# Используем пространство имен при поиске элементов
-info = root.find('ns:info', namespace)
-authors = info.find('ns:authors', namespace)
-author = authors.find('ns:author', namespace).text
-p.set_author(author)
+    # Используем пространство имен при поиске элементов
+    info = root.find('ns:info', namespace)
+    authors = info.find('ns:authors', namespace)
+    author = authors.find('ns:author', namespace).text
+    p.set_author(author)
 
-game = root.find('ns:rounds', namespace)
-for rnd in game.findall('ns:round', namespace):
-    R = Round(rnd.get('name'))
-    p.add_round(R)
-    topics = rnd.find('ns:themes', namespace)
-    for t in topics.findall('ns:theme', namespace):
-        T = Theme(t.get('name'))
-        R.add_theme(T)
-        qs = t.find('ns:questions', namespace)
-        for q in qs.findall('ns:question', namespace):
-            pr = q.get('price', namespace)
-            txt = None
-            im = None
-            snd = None
-            vd = None
-            scen = q.find('ns:scenario', namespace)
-            marker_flag = False
-            it = iter(scen.findall('ns:atom', namespace))
-            for atom in it:
-                if (tp := atom.get('type')):
-                    match tp:
-                        case 'image':
-                            im = atom.text
-                        case 'voice':
-                            snd = atom.text
-                        case 'video':
-                            vd = atom.text
-                        case 'marker':   #всё, что дальше - ответ
-                            marker_flag = True
-                            break
-                        case _:
-                            txt = atom.text
-            Q = Question(pr, txt, im, snd, vd)
-            if marker_flag:
-                r_ans = None
-                w_ans = None
+    game = root.find('ns:rounds', namespace)
+    for rnd in game.findall('ns:round', namespace):
+        R = Round(rnd.get('name'))
+        p.add_round(R)
+        topics = rnd.find('ns:themes', namespace)
+        for t in topics.findall('ns:theme', namespace):
+            T = Theme(t.get('name'))
+            R.add_theme(T)
+            qs = t.find('ns:questions', namespace)
+            for q in qs.findall('ns:question', namespace):
+                pr = q.get('price', namespace)
                 txt = None
                 im = None
                 snd = None
                 vd = None
+                scen = q.find('ns:scenario', namespace)
+                marker_flag = False
+                it = iter(scen.findall('ns:atom', namespace))
                 for atom in it:
                     if (tp := atom.get('type')):
                         match tp:
@@ -184,14 +161,39 @@ for rnd in game.findall('ns:round', namespace):
                                 snd = atom.text
                             case 'video':
                                 vd = atom.text
+                            case 'marker':   #всё, что дальше - ответ
+                                marker_flag = True
+                                break
                             case _:
                                 txt = atom.text
-            else:
-                r_ans = q.find('ns:right', namespace)
-                r_ans = r_ans.find('ns:answer', namespace).text
-                w_ans = q.find('ns:wrong', namespace)
-                if w_ans is not None:
-                    w_ans = w__ans.find('ns:answer', namespace).text  # надо найти пакет
-            ans = Answer(r_ans, w_ans, txt, im, snd, vd)
-            Q.add_answer(ans)
-            T.add_question(Q)
+                Q = Question(pr, txt, im, snd, vd)
+                if marker_flag:
+                    r_ans = None
+                    w_ans = None
+                    txt = None
+                    im = None
+                    snd = None
+                    vd = None
+                    for atom in it:
+                        if (tp := atom.get('type')):
+                            match tp:
+                                case 'image':
+                                    im = atom.text
+                                case 'voice':
+                                    snd = atom.text
+                                case 'video':
+                                    vd = atom.text
+                                case _:
+                                    txt = atom.text
+                else:
+                    r_ans = q.find('ns:right', namespace)
+                    r_ans = r_ans.find('ns:answer', namespace).text
+                    w_ans = q.find('ns:wrong', namespace)
+                    if w_ans is not None:
+                        w_ans = w__ans.find('ns:answer', namespace).text  # надо найти пакет
+                ans = Answer(r_ans, w_ans, txt, im, snd, vd)
+                Q.add_answer(ans)
+                T.add_question(Q)
+    return p
+
+p = parse_package(sys.argv[1])
